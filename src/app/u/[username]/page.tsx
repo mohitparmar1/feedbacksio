86cb6f0e69b374bd4f1314c17437a2b1a254a0bc
mohitparmar1 <contact.mohit06@gmail.com>
2025-01-25 16:19:22 +0530

    feat : profile page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -2,22 +2,194 @@
 import { messageSchema } from "@/schemas/messageSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useParams } from "next/navigation";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import { CardHeader, CardContent, Card } from "@/components/ui/card";
+import { toast } from "@/hooks/use-toast";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
 
-function page() {
+const specialChar = "||";
+
+const parseStringMessages = (messageString: string): string[] => {
+  return messageString ? messageString.split(specialChar) : [];
+};
+const initialMessageString =
+  "What's your favorite movie?||Do you have any pets?||What's your dream job?";
+function SendMessage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuggestLoading, setIsSuggestLoading] = useState(false);
+  const [suggestMessage, setSuggestMessage] = useState(initialMessageString);
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const params = useParams<{ username: string }>();
+  const username = params.username;
+
   const form = useForm<z.infer<typeof messageSchema>>({
     resolver: zodResolver(messageSchema),
   });
 
-  const onSubmit = async (data : z.infer<typeof messageSchema>) => {
+  const messageContent = form.watch("content");
+
+  const handleMessageClick = (message: string) => {
+    form.setValue("content", message);
+  };
+
+  const onSubmit = async (data: z.infer<typeof messageSchema>) => {
+    setIsLoading(true);
     try {
-      
+      const response = await axios.post<ApiResponse>("/api/send-message", {
+        ...data,
+        username,
+      });
+
+      toast({
+        title: response.data.message,
+        variant: "default",
+      });
+      form.reset({ ...form.getValues(), content: "" });
     } catch (error) {
-      
+      console.error("Error occurred while sending message", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description:
+          axiosError.response?.data.message ?? "Failed to send message",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
     }
   };
-  return <div>Message Page</div>;
+
+  const fetchSuggestedMessages = async () => {
+    setIsSuggestLoading(true);
+    try {
+      const result = await axios.post<ApiResponse>("/api/suggest-message");
+      const { message } = result.data;
+
+      if (message) {
+        setSuggestMessage(message);
+      } else {
+        throw new Error("No messages available.");
+      }
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+
+      toast({
+        title: "Error",
+        description: error.message || "Failed to fetch suggested messages.",
+        variant: "destructive",
+      });
+    } finally {
+      setIsSuggestLoading(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Public Profile Link
+      </h1>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Send Anonymous Message to @{username}</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center">
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button type="submit" disabled={isLoading || !messageContent}>
+                Send It
+              </Button>
+            )}
+          </div>
+        </form>
+      </Form>
+      <div className="space-y-4 my-8">
+        <div className="space-y-2">
+          <Button
+            onClick={fetchSuggestedMessages}
+            className="my-4"
+            disabled={isSuggestLoading}
+          >
+            {isSuggestLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Fetching...
+              </>
+            ) : (
+              "Suggest Messages"
+            )}
+          </Button>
+          <p>Click on any message below to select it.</p>
+        </div>
+        <Card>
+          <CardHeader>
+            <h3 className="text-xl font-semibold">Messages</h3>
+          </CardHeader>
+          <CardContent className="flex flex-col space-y-4">
+            {fetchError ? (
+              <p className="text-red-500">{fetchError}</p>
+            ) : (
+              parseStringMessages(suggestMessage).map((message, index) => (
+                <Button
+                  key={index}
+                  variant="outline"
+                  className="mb-2"
+                  onClick={() => handleMessageClick(message)}
+                >
+                  {message}
+                </Button>
+              ))
+            )}
+            {!suggestMessage && !fetchError && (
+              <p className="text-gray-500">No messages available.</p>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+      <Separator className="my-6" />
+      <div className="text-center">
+        <div className="mb-4">Get Your Message Board</div>
+        <Link href={"/sign-up"}>
+          <Button>Create Your Account</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default SendMessage;
